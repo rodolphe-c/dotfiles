93816a6bc59d259b4f73aa134e77d8a30ce80d0c
Rodolphe Cargnello <rodolphe.cargnello@gmail.com>
2024-03-26 21:04:06 +0100

    nvim + tmux

--- dot_config/nvim/lua/plugins/rosepine.lua
+++ dot_config/nvim/lua/plugins/rosepine.lua
@@ -69,7 +69,7 @@ return {
 			end,
 		})
 
-		vim.cmd("colorscheme rose-pine-dawn")
+		vim.cmd("colorscheme rose-pine-moon")
 		-- vim.cmd("colorscheme rose-pine-main")
 		-- vim.cmd("colorscheme rose-pine-moon")
 		-- vim.cmd("colorscheme rose-pine-dawn")
