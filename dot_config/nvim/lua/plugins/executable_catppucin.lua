949506e2f50c079fc527bd4c2d56ea646e658711
Rodolphe Cargnello <rodolphe.cargnello@gmail.com>
2024-05-01 23:32:32 +0200

    update nvim config

--- dot_config/nvim/lua/plugins/executable_catppucin.lua
+++ dot_config/nvim/lua/plugins/executable_catppucin.lua
@@ -1,17 +1,17 @@
 return {
 	'catppuccin/nvim',
-	enabled = false,
+	enabled = true,
 	cond = not vim.g.vscode,
 	name = 'catppuccin',
 	priority = 1000,
 	config = function()
 		require('catppuccin').setup({
-			flavour = 'latte', -- latte, frappe, macchiato, mocha
+			flavour = 'frappe', -- latte, frappe, macchiato, mocha
 			background = { -- :h background
 				light = 'latte',
 				dark = 'mocha',
 			},
-			transparent_background = true, -- disables setting the background color.
+			transparent_background = false, -- disables setting the background color.
 			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 			term_colors = false,  -- sets terminal colors (e.g. `g:terminal_color_0`)
 			dim_inactive = {
